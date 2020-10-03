2d91ae90cbd13d9580a78d279e17c313ab1709bb
Tadeusz <tadeuszjt@gmail.com>
2020-10-03 14:12:05 +0100

    merged

--- 32/angle.go
+++ 32/angle.go
@@ -1,14 +1,20 @@
 package geom
 
-import "math"
+import (
+    "math"
+    "math/rand"
+)
 
 type Angle float32
 
 func MakeAngle(rad float32) Angle {
-	mod := math.Mod(float64(rad), 2*math.Pi)
-	return Angle(mod)
+	return Angle(math.Mod(float64(rad), 2*math.Pi))
 }
 
-func (a Angle) Plus(b Angle) Angle {
-	return MakeAngle(float32(a + b))
+func AngleRand() Angle {
+    return MakeAngle(rand.Float32() * 2 * float32(math.Pi))
+}
+
+func (a Angle) Plus(rad float32) Angle {
+	return MakeAngle(float32(a) + rad)
 }
--- 32/ori2.go
+++ 32/ori2.go
@@ -3,7 +3,8 @@ package geom
 import "math"
 
 type Ori2 struct {
-	X, Y, Theta float32
+	X, Y  float32
+    Theta Angle
 }
 
 func (o Ori2) Vec2() Vec2 {
@@ -11,21 +12,17 @@ func (o Ori2) Vec2() Vec2 {
 }
 
 func (o Ori2) Vec3() Vec3 {
-	return Vec3{o.X, o.Y, o.Theta}
+	return Vec3{o.X, o.Y, float32(o.Theta)}
 }
 
 func (a Ori2) Times(b Ori2) Ori2 {
-	return Ori2{a.X * b.X, a.Y * b.Y, a.Theta * b.Theta}
+	return Ori2{a.X * b.X, a.Y * b.Y, MakeAngle(float32(a.Theta * b.Theta))}
 }
 
 func (a *Ori2) PlusEquals(b Ori2) {
 	a.X += b.X
 	a.Y += b.Y
-	a.Theta += b.Theta
-}
-
-func (o Ori2) ScaledBy(f float32) Ori2 {
-	return Ori2{f * o.X, f * o.Y, f * o.Theta}
+	a.Theta = MakeAngle(float32(a.Theta + b.Theta))
 }
 
 func (o Ori2) Mat3Transform() Mat3 {
